e4286c3e9b67a29cd7b9a49498aa7f3c238cca4b
xkevin190 <kevinvelasco193@gmail.com>
2023-04-26 18:04:58 -0400

    Initial Commit

---
+++ TestMaking/TestMakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMakingApp.swift
+//  TestMaking
+//
+//  Created by Kevin Velasco on 26/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TestMakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
